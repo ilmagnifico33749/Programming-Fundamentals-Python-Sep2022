a1da6f0e7004edf61761caddbf77f018a4cfe182
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-10-14 23:43:08 +0300

    14.10.2022 - 4

---
+++ 4. Lists/EX/2. Multiples List v1.py
@@ -0,0 +1,11 @@
+factor = int(input())
+count = int(input())
+multiplies_list = []
+for i in range(1,count + 1):
+    number = factor * i
+    multiplies_list.append(number)
+print(multiplies_list)
+
+
+
+
