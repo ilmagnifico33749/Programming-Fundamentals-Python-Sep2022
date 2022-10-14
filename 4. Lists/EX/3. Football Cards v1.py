87034d62e8f8b38d9c1d0fdbdc5746494e49d69f
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-10-15 02:52:46 +0300

    1

---
+++ 4. Lists/EX/3. Football Cards v1.py
@@ -0,0 +1,27 @@
+team_A = []
+team_B = []
+for index in range(1, 12):
+    symbol_A = "A-" + str(index)
+    team_A.append(symbol_A)
+    symbol_B = "B-" + str(index)
+    team_B.append(symbol_B)
+sequence_of_cards = input().split()
+for card in sequence_of_cards:
+    card_for_player = card.split("-")
+    if card[0] == "A":
+        for player in team_A:
+            player_no = player.split("-")
+            if player_no[1] == card[2]:
+                team_A.remove(player)
+                print(f"Removed {player}")
+    if card[0] == "B":
+        for player in team_B:
+            player_no = player.split("-")
+            if player_no[1] == card[2]:
+                team_B.remove(player)
+                print(f"Removed {player}")
+    if len(team_A) < 7 or len(team_B) < 7:
+        print(f"Team A - {len(team_A)}; Team B - {len(team_B)}")
+        break
+
+print(f"Team A - {len(team_A)}; Team B - {len(team_B)}")
