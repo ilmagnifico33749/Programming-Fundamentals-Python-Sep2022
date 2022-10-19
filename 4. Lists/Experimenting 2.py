c4fa0ad5073c080601ad896bc43d5fac60240357
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-10-19 20:37:46 +0300

    19.10.2022

--- 4. Lists/Experimenting 2.py
+++ 4. Lists/Experimenting 2.py
@@ -1,25 +1,18 @@
-list_all_gains = input().split(", ")
+list_all_gains = ["1", "2", "3", "4", "5"]
+reserve_list_all_gains = ["1", "2", "3", "4", "5"]
 number_of_traders = int(input())
 
-list_all_gains_integers = []
-for element in list_all_gains:
-    list_all_gains_integers.append(int(element))
+list_all_traders = ["Trader 1 gain", "Trader 2 gain", "Trader 3 gain"]
 
 list_final_gains = []
 starting_index = 0
 while starting_index < number_of_traders:
     gain_current_trader = 0
     for current_index in range(starting_index, len(list_all_gains), number_of_traders):
-        gain_current_trader += list_all_gains_integers[current_index]
+        gain_current_trader += list_all_gains[current_index]
     list_final_gains.append(gain_current_trader)
     starting_index += 1
 print(list_final_gains)
-
-
-
-
-
-
 
 
 
