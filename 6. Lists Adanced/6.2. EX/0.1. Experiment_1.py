e525788b48d4f6b821eede215e3faf24f893f6cd
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-11-11 01:54:21 +0200

    11/11/22 - 1

--- 6. Lists Adanced/6.2. EX/0.1. Experiment_1.py
+++ 6. Lists Adanced/6.2. EX/0.1. Experiment_1.py
@@ -1,8 +1,3 @@
-number_of_conf_rooms = int(input())
-count_free_chairs = 0
-for conf_room in range(number_of_conf_rooms):
-    conf_room_attendance = input().split()
-    print(len(conf_room_attendance[0]))
-    count_free_chairs += abs(len(conf_room_attendance[0]) - int(conf_room_attendance[1]))
-    print(conf_room_attendance)
-    print(count_free_chairs)
+chairs, attendants = input().split(" ")
+print(chairs)
+print(attendants)
--- 6. Lists Adanced/6.2. EX/5. Office Chairs v2.py
+++ 6. Lists Adanced/6.2. EX/5. Office Chairs v2.py
@@ -17,16 +17,21 @@
 #           2 more chairs needed in room 3  #
 #-------------------------------------------#
 
+def conf_room_control(total_number_conf_rooms):
+    count_free_chairs = 0
+    count_chairs_needed = 0
+    for room in range(1, total_number_conf_rooms + 1):
+        chairs, attendants = input().split(" ")
+        balance = len(chairs) - int(attendants)
+        if balance >= 0:
+            count_free_chairs += balance
+        else:
+            count_chairs_needed += abs(balance)
+            print(f"{abs(balance)} more chairs needed in room {room}")
+    return count_free_chairs, count_chairs_needed
+
 number_of_conf_rooms = int(input())
-count_free_chairs = 0
-for conf_room in range(1, number_of_conf_rooms + 1):
-    conf_room_attendance = input().split()
-    enough_chairs_everywhere = True
-    if len(conf_room_attendance[0]) < int(conf_room_attendance[1]):
-        enough_chairs_everywhere = False
-        print(f"{int(conf_room_attendance[1]) - len(conf_room_attendance[0])} more chairs"
-              f" needed in room {conf_room}")
-    else:
-        count_free_chairs += (abs(len(conf_room_attendance[0]) - int(conf_room_attendance[1])))
-if enough_chairs_everywhere == True:
-    print(f"Game On, {count_free_chairs} free chairs left")
+available_free_chairs, necessary_chairs = conf_room_control(number_of_conf_rooms)
+if available_free_chairs >= necessary_chairs:
+    print(f"Game On, {available_free_chairs} free chairs left")
+
