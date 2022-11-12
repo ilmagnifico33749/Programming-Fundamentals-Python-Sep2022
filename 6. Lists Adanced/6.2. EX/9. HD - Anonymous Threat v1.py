6e7407764b0b652f77a37b24fb5a6d9b6599a4d1
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-11-12 23:30:37 +0200

    12/11/22 - 4 - Satisf Deciph Problem Solved - Lists Advanced - Problem 9 - HD

---
+++ 6. Lists Adanced/6.2. EX/9. HD - Anonymous Threat v1.py
@@ -0,0 +1,92 @@
+#===============================================#
+# Input_1:                                      #
+# - Ivo Johny Tony Bony Mony                    #
+# - merge 0 3                                   #
+# - merge 3 4                                   #
+# - merge 0 3                                   #
+# - 3:1                                         #
+# Output: IvoJohnyTonyBonyMony                  #
+#-----------------------------------------------#
+# Input_2:                                      #
+# - abcd efgh ijkl mnop qrst uvwx yz            #
+# - merge 4 10                                  #
+# - divide 4 5                                  #
+# - 3:1                                         #
+#Output_2: abcd efgh ijkl mnop qr st uv wx yz   #
+#===============================================#
+
+import math
+
+encrypted_message = input().split(" ")
+
+command = input()
+while command != "3:1":
+    command_details = command.split()
+
+    if command_details[0] == "merge":
+        current_word = ""
+        #------------------------------------------#
+        #Times the numbers will actually be merged - BEGINNING#
+        actual_number_words_to_be_merged = 0
+        requested_number_words_to_merge = int(command_details[2]) - int(command_details[1]) + 1
+        possible_number_words_to_merge = len(encrypted_message) - (int(command_details[1]))
+        if requested_number_words_to_merge <= possible_number_words_to_merge:
+            actual_number_words_to_be_merged = requested_number_words_to_merge
+        else:
+            actual_number_words_to_be_merged = possible_number_words_to_merge
+        # Times the numbers will actually be merged - BEGINNING#
+        # ------------------------------------------#
+        current_word = str()
+        list_words_to_merge = []
+        for words in range(len(encrypted_message)):
+            for times in range(actual_number_words_to_be_merged):
+                if words == int(command_details[1]):
+                    current_word = encrypted_message.pop(int(command_details[1]))
+                    list_words_to_merge.append(current_word)
+        encrypted_message.insert(int(command_details[1]), ''.join(list_words_to_merge))
+
+    elif command_details[0] == "divide":
+        index = command_details[1]
+        word = encrypted_message.pop(int(index))
+        divider = math.floor(int(command_details[2]))
+        times_to_divide = int((int(len(word))) / int(divider))
+        remainder = len(word) % divider
+        if remainder == 0:
+            word_list = [x for x in word]
+            new_letter = []
+            for letter in range(len(word_list)):
+                if len(word_list) > 0:
+                    current_letter = ""
+                    list_current_letters = []
+                    for dividings in range(times_to_divide):
+                        current_letter = word_list.pop(0)
+                        list_current_letters.append(current_letter)
+                    new_letter.append(''.join(list_current_letters))
+            encrypted_message.insert(int(index), ' '.join(new_letter))
+
+        elif remainder > 0:
+            word_list = [x for x in word]
+            new_letter = []
+            first_part_len = int(times_to_divide * (math.floor(int(len(word_list)) / times_to_divide)) - times_to_divide)
+            while len(word_list) > (times_to_divide + remainder):
+                count_letters = times_to_divide
+                for letter in range(len(word_list)):
+                    if count_letters <= first_part_len:
+                        current_letter = ""
+                        list_current_letters = []
+                        for dividings in range(times_to_divide):
+                            count_letters += 1
+                            current_letter = word_list.pop(0)
+                            list_current_letters.append(current_letter)
+                        new_letter.append(''.join(list_current_letters))
+            else:
+                list_current_letters = []
+                current_letter = "".join(word_list)
+                list_current_letters.append(current_letter)
+                new_letter.append(''.join(list_current_letters))
+            encrypted_message.insert(int(index), ' '.join(new_letter))
+
+    command = input()
+
+else:
+    print(f"{' '.join(encrypted_message)}")
