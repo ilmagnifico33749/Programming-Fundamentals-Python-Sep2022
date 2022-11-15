abdba43a0894177ce29f7863f61b4cc2439fc33a
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-11-15 15:18:24 +0200

    15/11/22 - 1

--- 9. Objects and Classes/9.1. LAB/0.1. Experiment 1.py
+++ 9. Objects and Classes/9.1. LAB/0.1. Experiment 1.py
@@ -1,19 +1,3 @@
 
-class Person:
-
-    def __init__(self,name):
-        self.name = name
-
-    def say_hello(self):
-        print(f"Hello {self.name}")
-    def say_goodbye(self):
-        print(f"Goodbye {self.name}")
-    def say_welcome(self):
-        print(f"Welcome {self.name}")
-
-ivan = Person("Ivan")
-ivan.say_hello()
-
-print(ivan.say_welcome())
 
 
