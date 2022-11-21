979738d536208f3c141f9ed574723d1a779507ec
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-11-21 20:34:32 +0200

    .

--- 0. Private Projects for Fun/1. Password Identifier/Experiment 1.py
+++
@@ -1,3 +0,0 @@
-user_password = "Hello There"
-a = [x for x in user_password]
-print(a)
--- 0. Private Projects for Fun/1. Password Identifier/Experiment 2.py
+++
@@ -1,7 +0,0 @@
-user_password = "Hello There"
-password_min_length = 4
-
-
-len_password_to_generate = password_min_length
-password_to_generate = [0 for x in range(len_password_to_generate)]
-print(password_to_generate)
