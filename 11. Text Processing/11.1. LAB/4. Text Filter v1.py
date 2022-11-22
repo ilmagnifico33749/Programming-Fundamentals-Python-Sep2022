97c6d832ed763b73deaebcc104563ebe4fa410f6
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2022-11-22 20:52:40 +0200

    22/11/22 - 4

---
+++ 11. Text Processing/11.1. LAB/4. Text Filter v1.py
@@ -0,0 +1,10 @@
+banned_words = input().split(", ")
+text = input()
+
+for word in banned_words:
+    if word in text:
+        text = text.replace(word, (len(word) * "*"))
+
+print(text)
+
+# It is not Linux, it is GNU/Linux. Linux is merely the kernel, while GNU adds the functionality. Therefore we owe it to them by calling the OS GNU/Linux! Sincerely, a Windows client
