774827be4f9538979d638d36e6d04fbb25156272
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-09 21:28:30 +0200

    09/Jan/23 - 2

--- 12. Regular Expressions/12.2. EX/1. Capture the Numbers_v1.py
+++ 12. Regular Expressions/12.2. EX/1. Capture the Numbers_v1.py
@@ -1,21 +1,32 @@
 import re
 
 sequence = input()
-regex = "[]([0-9{+}])"
-list_filtered = []
 
-while sequence != "":
-    filtered = re.findall(regex, sequence)
-    print(filtered)
-    list_filtered.append(''.join(filtered))
+regex = "\d+"
+
+while sequence:
+    matches = re.findall(regex, sequence)
+    if matches:
+        print(' '.join(matches), end=" ")
     sequence = input()
 
-else:
-    print(f"{list_filtered}")
-
-
-
-# The300
-# What is that?
-# I think it's the 3rd movie
-# Let's watch it at 22:45
+##########################################
+# Input 1:
+#   The300
+#   What is that?
+#   I think it's the 3rd movie
+#   Let's watch it at 22:45
+# Output 1: 300 3 22 45
+##########################################
+# Input 2:
+#   123a456
+#   789b987
+#   654c321
+#   0
+# Output 2: 123 456 789 987 654 321 0
+##########################################
+# Input 3:
+# Let's go11!!!11!
+# Okey!1!
+# Output 3: 11 11 1
+##########################################
