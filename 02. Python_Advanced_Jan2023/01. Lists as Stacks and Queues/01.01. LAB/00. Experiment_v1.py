3dbf407d0485a39de6daf4b8ea691b97c74d31fe
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-16 15:33:27 +0200

    16/Jan/23 - 4

--- 02. Python_Advanced_Jan2023/01. Lists as Stacks and Queues/01.01. LAB/00. Experiment_v1.py
+++ 02. Python_Advanced_Jan2023/01. Lists as Stacks and Queues/01.01. LAB/00. Experiment_v1.py
@@ -1,12 +1,18 @@
-from _collections import  deque
+# from _collections import  deque
+#
+# example_q = deque()
+#
+# example_q.append(1)
+# example_q.append(2)
+# example_q.append(3)
+# example_q.append(4)
+#
+# # while example_q:
+# example_q.popleft()
+# print(example_q)
 
-example_q = deque()
 
-example_q.append(1)
-example_q.append(2)
-example_q.append(3)
-example_q.append(4)
-
-# while example_q:
-example_q.popleft()
-print(example_q)
+a = input()
+b = a.split()
+print(b)
+print(b[0])
---
+++ 02. Python_Advanced_Jan2023/01. Lists as Stacks and Queues/01.01. LAB/04. Water_Dispenser_v1.py
@@ -0,0 +1,63 @@
+from _collections import deque
+
+water_available = int(input())
+my_queue_1_people = deque()
+command = input()
+
+while command != "Start":
+    my_queue_1_people.append(command)
+    command = input()
+else:
+    command_two = input()
+    while command_two != "End":
+        command_details = command_two.split()
+        if len(command_details) == 1:
+            water_to_consume = int(command_details[0])
+            if water_to_consume <= water_available:
+                water_available -= water_to_consume
+                print(f"{my_queue_1_people.popleft()} got water")
+            else:
+                print(f"{my_queue_1_people.popleft()} must wait")
+        elif len(command_details) == 2:
+            if command_details[0] == "refill":
+                water_available += int(command_details[1])
+        command_two = input()
+    else:
+        print(f"{water_available} liters left")
+
+# ######################
+# Input_1:
+# 2
+# Peter
+# Amy
+# Start
+# 2
+# refill 1
+# 1
+# End
+# ---------------------
+# Output_1:
+# Peter got water
+# Amy got water
+# 0 liters left
+# ######################
+# Input_2:
+# 10
+# Peter
+# George
+# Amy
+# Alice
+# Start
+# 2
+# 3
+# 3
+# 3
+# End
+# ---------------------
+# Output_2:
+# Peter got water
+# George got water
+# Amy got water
+# Alice must wait
+# 2 liters left
+# ######################
