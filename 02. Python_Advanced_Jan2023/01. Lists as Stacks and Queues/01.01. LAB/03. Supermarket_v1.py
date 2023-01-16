21399904a7c3f9c6da34a7d034ab310742c70b0c
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-16 13:13:41 +0200

    16/Jan/23 - 3

--- 02. Python_Advanced_Jan2023/01. Lists as Stacks and Queues/01.01. LAB/03. Supermarket_v1.py
+++ 02. Python_Advanced_Jan2023/01. Lists as Stacks and Queues/01.01. LAB/03. Supermarket_v1.py
@@ -1,2 +1,45 @@
-from _collections import  deque
+from collections import deque
 
+my_queue = deque()
+
+command = input()
+
+while command != "End":
+    if command != "Paid":
+        my_queue.append(command)
+    else:
+        while my_queue:
+            print(my_queue.popleft())
+
+    command = input()
+
+else:
+    print(f"{len(my_queue)} people remaining.")
+
+# ##########################
+# Input_1:
+# George
+# Peter
+# William
+# Paid
+# Michael
+# Oscar
+# Olivia
+# Linda
+# End
+# --------------------------
+# Output_1:
+# George
+# Peter
+# William
+# 4 people remaining.
+# ##########################
+# Input_2:
+# Anna
+# Emma
+# Alexander
+# End
+# --------------------------
+# Output_2:
+# 3 people remaining.
+# ##########################
