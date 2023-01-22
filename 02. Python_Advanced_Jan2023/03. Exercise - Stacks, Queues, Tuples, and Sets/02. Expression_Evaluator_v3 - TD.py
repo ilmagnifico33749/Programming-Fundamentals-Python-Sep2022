6b59807105adc26be4177aaaf8fef524fa91e544
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-22 02:02:41 +0200

    22/Jan/23 - 2

--- 02. Python_Advanced_Jan2023/03. Exercise - Stacks, Queues, Tuples, and Sets/02. Expression_Evaluator_v3 - TD.py
+++ 02. Python_Advanced_Jan2023/03. Exercise - Stacks, Queues, Tuples, and Sets/02. Expression_Evaluator_v3 - TD.py
@@ -1,7 +1,13 @@
+# This is the code for the v2 solution of the problem, not the final version but the draft
+# with the comments.
+# To write  a different, desirably more optimized solution
+
 from _collections import deque
 import math
 
 single_line_expression = input()
+# single_line_expression = "6 3 - 2 1 * 5 /"
+# single_line_expression = "10 23 * 4 2 / 30 10 + 100 50 - 2 -1 *"
 queue_whole_expression = single_line_expression.split(" ")
 current_operator_index = 0
 current_operator = ""
@@ -39,19 +45,34 @@ def math_expr_calc(operator, numbers, ongoing_sum, first_time_status):
 
     return final_result
 
+print(queue_whole_expression)
+print(50 * "#")
+print(50 * "#")
+
 while queue_whole_expression:
     for symbols in range(len(queue_whole_expression)):
         symbol = queue_whole_expression[symbols]
+        print(f"Current_symbol: {symbol}")
+        print(f"Len Current Symbol = {len(symbol)}")
+        print(50 * "-")
         if len(symbol) == 1 and symbol.isnumeric() == False:
+            print(50 * "-")
             current_operator_index = symbols
             current_operator = queue_whole_expression.pop(symbols)
+            print(f"Current Operator: {current_operator} with index {symbols}")
+            print(f"Current Queue: {queue_whole_expression}")
             current_numbers = [queue_whole_expression.pop(0) for x in queue_whole_expression[0:current_operator_index]]
+            print(f"Current Numbers: {current_numbers}")
+            if first_time == False:
+                print(f"Previous current_ongoing_sum: {current_sum}")
             final_sum = math_expr_calc(current_operator, current_numbers, current_sum, first_time)
+            print(f"New Current_ongoing_sum: {final_sum}")
             current_sum = final_sum
             first_time = False
+            print(50 * "#")
             break
 
-print(final_sum)
+print(f"Final Sum: {final_sum}")
 # ###
 # Input_1:
 # 6 3 - 2 1 * 5 /
