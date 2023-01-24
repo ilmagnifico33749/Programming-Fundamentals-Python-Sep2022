41530d54420037c4ce29c8d1dfc29fdab46db70f
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-24 16:16:43 +0200

    24/Jan/23 - 1

---
+++ 02. Python_Advanced_Jan2023/04. Multidimensional Lists/04.01. LAB/01. Sum_Matrix_Elements_v1.py
@@ -0,0 +1,22 @@
+rows, columns = [int(x) for x in input().split(", ")]
+
+matrix = []
+
+for row in range(rows):
+    matrix.append([int(x) for x in input().split(", ")])
+
+print(sum([sum(matrix[index]) for index in range(len(matrix))]))
+print(matrix)
+
+# ###
+# Input_1:
+# 3, 6
+# 7, 1, 3, 3, 2, 1
+# 1, 3, 9, 8, 5, 6
+# 4, 6, 7, 9, 1, 0
+# ---
+# Output:
+# 76
+# [[7, 1, 3, 3, 2, 1], [1, 3, 9, 8, 5, 6], [4, 6, 7, 9, 1, 0]]
+# ###
+
