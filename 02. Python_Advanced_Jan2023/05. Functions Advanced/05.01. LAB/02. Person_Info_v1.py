f4d982d4dc2d7639cca793b53e98ac5b580817a9
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-30 23:23:49 +0200

    30/Jan/23 - 2

---
+++ 02. Python_Advanced_Jan2023/05. Functions Advanced/05.01. LAB/02. Person_Info_v1.py
@@ -0,0 +1,11 @@
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+# ##################################################################
+# Test_Code_1:
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+# ------------------------------------------------------------------
+# Output_1:
+# This is George from Sofia and he is 20 years old
+# ##################################################################
