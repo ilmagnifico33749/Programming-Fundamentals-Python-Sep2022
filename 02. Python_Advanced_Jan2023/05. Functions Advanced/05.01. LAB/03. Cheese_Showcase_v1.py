a242cbe57000682a8228b214c25b1ad70fd8482e
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-01-30 23:58:50 +0200

    30/Jan/23 - 3

---
+++ 02. Python_Advanced_Jan2023/05. Functions Advanced/05.01. LAB/03. Cheese_Showcase_v1.py
@@ -0,0 +1,58 @@
+def sorting_cheeses(**cheeses_dict):
+    cheeses_dict = sorted(
+        cheeses_dict.items(),
+        key=lambda x: (-len(x[1]), x[0]))
+
+    result = []
+
+    for (cheese_name, quantities) in cheeses_dict:
+        result.append(cheese_name)
+        quantity_list = sorted(quantities, reverse=True)
+        result += quantity_list
+
+    return  "\n".join([str(x) for x in result])
+
+
+# ###################################
+# Test_Code_1:
+# print(
+#     sorting_cheeses(
+#         Parmesan=[102, 120, 135],
+#         Camembert=[100, 100, 105, 500, 430],
+#         Mozzarella=[50, 125],
+#     )
+# )
+# ------------------------------------
+# Output_1:
+# Camembert
+# 500
+# 430
+# 105
+# 100
+# 100
+# Parmesan
+# 135
+# 120
+# 102
+# ###################################
+# Test_Code_2:
+# print(
+#
+# sorting_cheeses(
+#         Parmigiano=[165, 215],
+#         Feta=[150, 515],
+#         Brie=[150, 125]
+#     )
+# )
+# ------------------------------------
+# Output_2:
+# Brie
+# 150
+# 125
+# Feta
+# 515
+# 150
+# Parmigiano
+# 215
+# 165
+# ###################################
