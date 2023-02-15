27463f86a86a23e8a42eff715c243c044a3e0de8
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-02-15 19:37:26 +0200

    15/Feb/23 - 2

---
+++ 02. Python_Advanced_Jan2023/05. Functions Advanced/05.02. EX_1/11. HD - Math_Operations_v1.py
@@ -0,0 +1,72 @@
+from collections import deque
+import math
+
+def math_operations(*args, **kwargs):
+    queue_floats = deque(args)
+    my_dict = kwargs
+
+    def math_actions(command, num1, num2):
+        result = float()
+        if command == "a":
+            result = num1 + num2
+        elif command == "s":
+            result = num1 - num2
+        elif command == "d":
+            if num1 != 0 and num2 != 0:
+                result = num1 / num2
+            else:
+                result = float(num1)
+        elif command == "m":
+            result = num1 * num2
+        return result
+
+
+    def new_func(queue_numbers, my_dictionary):
+        queue_numbers = queue_numbers
+        for keys, values in my_dictionary.items():
+            if len(queue_floats) > 0:
+                my_dictionary[keys] = math_actions(keys, my_dict[keys], queue_numbers.popleft())
+
+        if len(queue_numbers) > 0:
+            return new_func(queue_numbers, my_dictionary)
+        else:
+
+            for keys, values in my_dictionary.items():
+                my_dictionary[keys] = round(float(my_dictionary[keys]), 1)
+
+            return '\n'.join(
+                [(': '.join([str(x) for x in y])) for y in sorted(my_dictionary.items(), key=lambda x: (-x[1], x[0]))])
+
+    return new_func(queue_floats, my_dict)
+
+
+# ####################################################################################
+# Test_Code_1:
+print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+print(50 * '-')
+# ------------------------------------------------------------------------------------
+# Output_1:
+# d: 33.0
+# s: 15.1
+# a: 9.1
+# m: -58.5
+# ####################################################################################
+# Test_Code_2:
+print(math_operations(-1.0, 0.5, 1.6, 0.5, 6.1, -2.8, 80.0, a=0, s=(-2.3), d=0, m=0))
+print(50 * '-')
+# ------------------------------------------------------------------------------------
+# a: 5.1
+# d: 0.0
+# m: 0.0
+# s: 0.0
+# ####################################################################################
+# Test_Code_3:
+print(math_operations(6.0, a=0, s=0, d=5, m=0))
+print(50 * '-')
+# ------------------------------------------------------------------------------------
+# Output_1:
+# a: 6.0
+# d: 5.0
+# m: 0.0
+# s: 0.0
+# ####################################################################################
