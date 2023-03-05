f7cb7a0d655b97c87e3fc251d29fb2382c4de8b8
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-03-05 23:42:00 +0200

    05/Mar/23 - 3

--- 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/section.py
+++ 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/section.py
@@ -1,2 +1,76 @@
+# from task import Task
+from project.task import Task
+
+
 class Section:
-    pass
+    def __init__(self, name: str):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task: Task):
+        # for current_task in self.tasks:
+        #     if current_task.details() == new_task.details():
+        #         return f"Task {new_task.details()} is already in the section {self.name}"
+        try:
+            current_task = next(filter(lambda t: t.details() == new_task.details(), self.tasks))
+        except StopIteration:
+            self.tasks.append(new_task)
+            return f"Task {new_task.details()} is added to the section"
+
+        return f"Task is already in the section {self.name}"
+
+    def complete_task(self, task_name: str):
+        try:
+            searched_task = next(filter(lambda t: t.name == task_name, self.tasks))
+        except StopIteration:
+            return f"Could not find task with the name {task_name}"
+
+        self.tasks[self.tasks.index(searched_task)].completed = True
+        return f"Completed task {task_name}"
+
+    def clean_section(self):
+        completed_tasks = [specific_task.name for specific_task in self.tasks if specific_task.completed == True]
+        [[self.tasks.remove(current_task) for current_task in self.tasks if current_task.name == specific_task]
+         for specific_task in completed_tasks]
+        return f"Cleared {len(completed_tasks)} tasks."
+
+    def view_section(self):
+        tasks_details = '\n'.join([final_task.details() for final_task in self.tasks])
+        return f"Section {self.name}:\n" \
+               f"{tasks_details}"
+
+
+##########################################################################
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+# print(50 * '-')
+# third_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+# print(section.add_task(third_task))
+# print(50 * '-')
+# print(section.complete_task("Make bed"))
+# print(50 * '-')
+# print(section.complete_task("Go to University"))
+# print(section.complete_task(second_task.name))
+# print(50 * '-')
+print(section.clean_section())
+print(section.view_section())
+# --------------------------------------------------------------------------
+# Output_1:
+# Go to University
+# 28.05.2020
+# Don't forget laptop and notebook
+# Name: Go to University - Due Date: 28.05.2020
+# Task Name: Go to University - Due Date: 28.05.2020 is added to the section
+# Cleared 0 tasks.
+# Section Daily tasks:
+# Name: Go to University - Due Date: 28.05.2020
+# Name: Make bed - Due Date: 27/05/2020
+# ###########################################################################
--- 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/task.py
+++ 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/task.py
@@ -1,23 +1,51 @@
 
 class Task:
+    def __init__(self, name: str, due_date: str):
+        self.name = name
+        self.due_date = due_date
+        self.comments = []
+        self.completed = False
 
+    def change_name(self, new_name: str):
+        if new_name == self.name:
+            return f"Name cannot be the same."
+        self.name = new_name
+        return new_name
 
+    def change_due_date(self, new_date: str):
+        if self.due_date == new_date:
+            return f"Date cannot be the same."
+        self.due_date = new_date
+        return new_date
+
+    def add_comment(self, comment: str):
+        self.comments.append(comment)
+
+    def edit_comment(self, comment_number: int, new_comment: str):
+        try:
+            self.comments[comment_number] = new_comment
+            return f"{', '.join(self.comments)}"
+        except IndexError:
+            return f"Cannot find comment."
+
+    def details(self):
+        return f"Name: {self.name} - Due date: {self.due_date}"
 
 
 # #######################################################################
 # Test_Code_1:
-task = Task("Make bed", "27/05/2020")
-print(task.change_name("Go to University"))
-print(task.change_due_date("28.05.2020"))
-task.add_comment("Don't forget laptop")
-print(task.edit_comment(0, "Don't forget laptop and notebook"))
-print(task.details())
-section = Section("Daily tasks")
-print(section.add_task(task))
-second_task = Task("Make bed", "27/05/2020")
-section.add_task(second_task)
-print(section.clean_section())
-print(section.view_section())
+# task = Task("Make bed", "27/05/2020")
+# print(task.change_name("Go to University"))
+# print(task.change_due_date("28.05.2020"))
+# task.add_comment("Don't forget laptop")
+# print(task.edit_comment(0, "Don't forget laptop and notebook"))
+# print(task.details())
+# section = Section("Daily tasks")
+# print(section.add_task(task))
+# second_task = Task("Make bed", "27/05/2020")
+# section.add_task(second_task)
+# print(section.clean_section())
+# print(section.view_section())
 # --------------------------------------------
 # Output_1:
 # Go to University
