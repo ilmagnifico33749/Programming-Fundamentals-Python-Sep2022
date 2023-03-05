58361d5196ab539e338f716563854577c07f492a
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-03-06 00:09:51 +0200

    06/Mar/23 - 00 - Corrections

--- 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/task.py
+++ 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/05. To-do_List_v1/project/task.py
@@ -1,5 +1,5 @@
-
 class Task:
+
     def __init__(self, name: str, due_date: str):
         self.name = name
         self.due_date = due_date
@@ -7,16 +7,16 @@ class Task:
         self.completed = False
 
     def change_name(self, new_name: str):
-        if new_name == self.name:
+        if self.name == new_name:
             return f"Name cannot be the same."
         self.name = new_name
-        return new_name
+        return self.name
 
     def change_due_date(self, new_date: str):
         if self.due_date == new_date:
             return f"Date cannot be the same."
         self.due_date = new_date
-        return new_date
+        return self.due_date
 
     def add_comment(self, comment: str):
         self.comments.append(comment)
@@ -29,7 +29,7 @@ class Task:
             return f"Cannot find comment."
 
     def details(self):
-        return f"Name: {self.name} - Due date: {self.due_date}"
+        return f"Name: {self.name} - Due Date: {self.due_date}"
 
 
 # #######################################################################
