6ed9e939ac25454ae7f93146964fa8c5849825b8
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-03-06 22:34:29 +0200

    06/Mar/23 - 1

---
+++ 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/07. Spoopify_v1/project/album.py
@@ -0,0 +1,52 @@
+# from song import Song
+from project.song import Song
+
+
+class Album:
+    def __init__(self, name: str, songs):
+        self.name = name
+        self.published = False
+        self.songs = [songs]
+
+    def add_song(self, song: Song):
+        if song.single is True:
+            return f"Cannot add {song.name}. It's a single"
+
+        if self.published is True:
+            return f"Cannot add songs. Album is published."
+
+        if song in self.songs:
+            return f"Song is already in the album."
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name):
+        try:
+            searched_song = next(filter(lambda s: s.name == song_name, self.songs))
+            if self.published is True:
+                return f"Cannot remove songs. Album is published."
+            self.songs.remove(searched_song)
+            return f"Removed song {song_name} from album {self.name}."
+        except StopIteration:
+            return f"Song is not in the album."
+
+    def publish(self):
+        if self.published is True:
+            return f"Album {self.name} is already published."
+        self.published = True
+        return f"Album {self.name} has been published."
+
+    def details(self):
+        # Start - To do properly
+        all_songs = '\n== '.join(current_song.get_info() for current_song in self.songs)
+        # End
+
+        return f"Album {self.name}" \
+               f"\n== {all_songs}"
+
+
+
+
+
+
+
---
+++ 03. Python_OOP_Feb2023/02. Classes and Objects/02. EX/07. Spoopify_v1/project/band.py
@@ -0,0 +1,63 @@
+# from song import Song
+# from album import Album
+from project.album import Album
+from project.song import Song
+
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str):
+        try:
+            searched_album = next(filter(lambda a: a.name == album_name, self.albums))
+            if searched_album.published is True:
+                return f"Album has been published. It cannot be removed."
+            self.albums.remove(searched_album)
+            return f"Album {album_name} has been removed."
+        except StopIteration:
+            return f"Album {album_name} is not found."
+
+    def details(self):
+        all_albums = '\n'.join(current_album.details() for current_album in self.albums)
+        return f"Band {self.name}" \
+               f"\n{all_albums}"
+
+
+
+# ############################################################
+# Test_Output_1:
+song = Song("Running in the 90s", 3.45, False)
+print(song.get_info())
+album = Album("Initial D", song)
+second_song = Song("Around the World", 2.34, False)
+print(album.add_song(second_song))
+print(album.details())
+print(album.publish())
+band = Band("Manuel")
+print(band.add_album(album))
+print(band.remove_album("Initial D"))
+print(band.details())
+# -------------------------------------------------------------
+# Output_1:
+# Running in the 90s - 3.45
+# Song Around the World has been added to the album Initial D.
+# Album Initial D
+# == Running in the 90s - 3.45
+# == Around the World - 2.34
+#
+# Album Initial D has been published.
+# Band Manuel has added their newest album Initial D.
+# Album has been published. It cannot be removed.
+# Band Manuel
+# Album Initial D
+# == Running in the 90s - 3.45
+# == Around the World - 2.34
+# ############################################################
