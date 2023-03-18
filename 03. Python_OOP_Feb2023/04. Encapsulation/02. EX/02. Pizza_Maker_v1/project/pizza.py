50f481da7ec4094fa861bcd2971f74018fb7600d
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-03-18 23:26:42 +0200

    18-Mar-23 / 2

---
+++ 03. Python_OOP_Feb2023/04. Encapsulation/02. EX/02. Pizza_Maker_v1/project/pizza.py
@@ -0,0 +1,70 @@
+from topping import Topping
+from dough import Dough
+
+class Pizza:
+    def __init__(self, name: str, dough: Dough, max_number_of_toppings: int, toppings: dict={} ):
+        self.name = name
+        self.dough = dough
+        self.max_number_of_toppings = max_number_of_toppings
+        self.toppings = toppings
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("The name cannot be an empty string")
+        self.__name = value
+
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        if value == None:
+            return "You should add dough to the pizza"
+        self.__dough = value
+
+
+    @property
+    def max_number_of_toppings(self):
+        return self.__max_number_of_toppings
+
+    @max_number_of_toppings.setter
+    def max_number_of_toppings(self, value):
+        if value <= 0:
+            raise ValueError("The maximum number of toppings cannot be less or equal to zero")
+        self.__max_number_of_toppings = value
+
+
+    @property
+    def toppings(self):
+        return self.__toppings
+
+    @toppings.setter
+    def toppings(self, value):
+        self.__toppings = dict()
+        # {Topping_Type: Topping_Weight}
+
+
+
+    def add_topping(self, topping: Topping):
+        if self.__max_number_of_toppings >= len(self.__toppings):
+            return f"Not enough space for another topping"
+        else:
+            if topping in self.__toppings:
+                self.__toppings[topping] += topping.weight
+            else:
+                self.__toppings[topping.topping_type] = topping.weight
+
+    def calculate_total_weight(self):
+        total_weight = self.__dough.weight
+        for key in self.__toppings.keys():
+            total_weight += self.__toppings[key]
+        return total_weight
+
