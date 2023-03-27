1415f51dc9ba53ca4264f1498f30808471e2c563
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-03-27 15:25:48 +0300

    27/Mar/23 - 1

---
+++ 03. Python_OOP_Feb2023/05. Static and Class Methods/02. EX/03. Document_Management_v1/project/storage.py
@@ -0,0 +1,66 @@
+# from topic import Topic
+# from category import Category
+# from document import Document
+
+from project.topic import Topic
+from project.category import Category
+from project.document import Document
+
+class Storage:
+    def __init__(self):
+        self.categories = list()
+        self.topics = list()
+        self.documents = list()
+
+    def add_category(self, category: Category):
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def add_topic(self, topic: Topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_document(self, document: Document):
+        if document not in self.documents:
+            self.documents.append(document)
+
+    def edit_category(self, category_id: int, new_name: str):
+        for category in self.categories:
+            if category.id == category_id:
+                category.edit(new_name)
+
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
+        for topic in self.topics:
+            if topic.id == topic_id:
+                topic.edit(new_topic, new_storage_folder)
+
+    def edit_document(self, document_id: int, new_file_name: str):
+        for document in self.documents:
+            if document.id == document_id:
+                document.edit(new_file_name)
+
+    def delete_category(self, category_id):
+        for category in self.categories:
+            if category.id == category_id:
+                self.categories.remove(category)
+
+    def delete_topic(self, topic_id):
+        for topic in self.topics:
+            if topic.id == topic_id:
+                self.topics.remove(topic)
+
+    def delete_document(self, document_id):
+        for document in self.documents:
+            if document.id == document_id:
+                self.documents.remove(document)
+
+    def get_document(self, document_id):
+        for document in self.documents:
+            if document.id == document_id:
+                return document
+
+    def __repr__(self):
+        return '\n'.join([document.__repr__() for document in self.documents])
+
+
+
