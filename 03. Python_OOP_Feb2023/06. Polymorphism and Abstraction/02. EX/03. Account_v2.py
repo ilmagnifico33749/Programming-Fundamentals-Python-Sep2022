91e2f1eb075c8d9fe84b0861befd495d6e3edbe3
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-04-01 19:19:13 +0300

    01/Apr/23 - 3

--- 03. Python_OOP_Feb2023/06. Polymorphism and Abstraction/02. EX/03. Account_v2.py
+++ 03. Python_OOP_Feb2023/06. Polymorphism and Abstraction/02. EX/03. Account_v2.py
@@ -26,7 +26,6 @@ class Account:
         new_account._transactions = self._transactions + other._transactions
         return new_account
 
-    # Below follow comparative function
     def __str__(self):
         return f"Account of {self.owner} with starting amount: {self.amount}"
 
@@ -36,30 +35,20 @@ class Account:
     def __len__(self):
         return len(self._transactions)
 
-    def __getitem__(self, transaction_index):
-        return f"{self._transactions[transaction_index]}"
+    def __getitem__(self, idx):
+        return self._transactions[idx]
 
     def __reversed__(self):
-        # not sure whether only to reverse the order or also to return it
         return reversed(self._transactions)
-
-    def __lt__(self, other):
-        return self.balance < other.balance
-
-    def __le__(self, other):
-        return self.balance <= other.balance
-
-    def __eq__(self, other):
-        return self.balance == other.balance
-
-    def __ne__(self, other):
-        return self.balance != other.balance
 
     def __gt__(self, other):
         return self.balance > other.balance
 
     def __ge__(self, other):
         return self.balance >= other.balance
+
+    def __eq__(self, other):
+        return self.balance == other.balance
 
 
 # ###################################################
@@ -69,6 +58,7 @@ acc2 = Account('john')
 print(acc)
 print(repr(acc))
 acc.add_transaction(20)
+print(acc._transactions)
 acc.add_transaction(-20)
 acc.add_transaction(30)
 print(acc.balance)
@@ -114,3 +104,4 @@ print(acc != acc2)
 # Account of bob&john with starting amount: 10
 # [20, -20, 30, 10, 60]
 # ###################################################
+
