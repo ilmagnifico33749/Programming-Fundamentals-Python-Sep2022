5831d25db324764c2f1c19e548e6b081bad50e8f
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-04-03 00:54:04 +0300

    03/Apr/23 - 2

---
+++ 03. Python_OOP_Feb2023/07. SOLID/01. LAB/02. Animals_v1.py
@@ -0,0 +1,68 @@
+# ####################################################
+# # Code_To_Correct_1:
+# class Animal:
+#     def __init__(self, species):
+#         self.species = species
+#
+#     def get_species(self):
+#         return self.species
+#
+#
+# def animal_sound(animals: list):
+#     for animal in animals:
+#         if animal.species == 'cat':
+#             print('meow')
+#         elif animal.species == 'dog':
+#             print('woof-woof')
+#
+#
+# animals = [Animal('cat'), Animal('dog')]
+# animal_sound(animals)
+#
+# ## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
+# ## при добавяне на нови животни
+# # animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
+# ####################################################
+# Corrected_Code_1:
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def animal_sound():
+        pass
+
+
+class Cat(Animal):
+    @staticmethod
+    def animal_sound():
+        return "Meow!"
+
+
+class Dog(Animal):
+    @staticmethod
+    def animal_sound():
+        return "Woof!"
+
+
+class Duck(Animal):
+    @staticmethod
+    def animal_sound():
+        return "Quack!"
+
+####################################################
+# Test_1:
+animal = Animal
+cat = Cat
+dog = Dog
+duck = Duck
+print(animal.animal_sound())
+print(cat.animal_sound())
+print(dog.animal_sound())
+print(duck.animal_sound())
+####################################################
