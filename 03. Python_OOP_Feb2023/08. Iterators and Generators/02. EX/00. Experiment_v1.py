43fb81cfb62045f813adcdb3bdaf7141a8a6008d
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-04-05 23:19:37 +0300

    05/Apr/23 - 4

---
+++ 03. Python_OOP_Feb2023/08. Iterators and Generators/02. EX/00. Experiment_v1.py
@@ -0,0 +1,3 @@
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
---
+++ 03. Python_OOP_Feb2023/08. Iterators and Generators/02. EX/01. Take_Skip_v1.py
@@ -0,0 +1,48 @@
+class take_skip:
+    def __init__(self, step: int, count: int):
+        self.step = step
+        self.count = count
+        self.current_index = 0
+        self.current_value = ...
+        # self.iterable_object = [x for x in range(0, self.count+1, self.step)]
+        self.iterable_object = [x * self.step for x in range(self.count)]
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_index <= (len(self.iterable_object) - 1):
+            self.current_value = self.iterable_object[self.current_index]
+            self.current_index += 1
+            return self.current_value
+        else:
+            raise StopIteration
+
+
+# ##################################
+# Test_Code_1:
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+# ---------------------------------
+# Output_1:
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
+# ##################################
+# Test_Code_1:
+# numbers = take_skip(10, 5)
+# for number in numbers:
+#     print(number)
+# ---------------------------------
+# Output_1:
+# 0
+# 10
+# 20
+# 30
+# 40
+# ##################################
