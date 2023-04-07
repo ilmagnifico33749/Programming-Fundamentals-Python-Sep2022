8592b0091718fc3d9de350c1c0bb64d5c3c66025
ilmagnifico33749 <96083262+ilmagnifico33749@users.noreply.github.com>
2023-04-07 22:35:04 +0300

    07/Apr/23 - 2

--- 03. Python_OOP_Feb2023/09. Decorators/01. LAB/00. Experiment_v1.py
+++ 03. Python_OOP_Feb2023/09. Decorators/01. LAB/00. Experiment_v1.py
@@ -1,7 +1,28 @@
-def print_message(message):
-    def message_sender():
-        # "Nested Fuction"
-        print(message)
-    message_sender()
+from functools import wraps
 
-print_message("Some random message")
+def uppercase(function):
+    @wraps(function)
+    def wrapper():
+        result = function()
+        uppercase_result = result.upper()
+        return uppercase_result
+
+    return wrapper
+
+#
+# def say_hi():
+#     return 'hello there'
+#
+# decorate = uppercase(say_hi())
+#
+# # decorate()
+#
+# print(decorate)
+
+@uppercase
+def say_hi():
+    return 'hello there'
+print(str(say_hi()))
+
+print(say_hi.__name__)
+print(say_hi.__doc__)
