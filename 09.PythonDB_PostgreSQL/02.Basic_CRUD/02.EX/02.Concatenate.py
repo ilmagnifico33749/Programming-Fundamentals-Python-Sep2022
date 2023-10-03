3f1e8ec968c555ac9ca04b4948a9934690a4e71c
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2023-10-03 23:01:11 +0300

    03/Oct/23 - 2

---
+++ 09.PythonDB_PostgreSQL/02.Basic_CRUD/02.EX/02.Concatenate.py
@@ -0,0 +1,5 @@
+SELECT
+CONCAT(name, ' ', state) as "Cities Information",
+area as "Area"
+FROM cities
+ORDER BY id ASC;
---
+++ 09.PythonDB_PostgreSQL/02.Basic_CRUD/02.EX/03.Remove_Duplicate_Rows.py
@@ -0,0 +1,3 @@
+]SELECT DISTINCT ON (name) name, area AS "Area (km2)"
+FROM cities
+ORDER BY name DESC;
